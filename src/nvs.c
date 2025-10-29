77bc1ec6781cf4be3ddd6f0ee246d0d85819c68c
itlantu <it_lantu@outlook.com>
2025-10-29 21:20:12 +0800

    feat: 新增nvs读写全局配置函数的实现, 未测试

--- src/nvs.c
+++ src/nvs.c
@@ -1,4 +1,5 @@
 #include "wi/nvs.h"
+#include <string.h>
 #include "esp_log.h"
 #include "nvs_flash.h"
 
@@ -20,4 +21,42 @@ esp_err_t wi_nvs_init() {
 	nvs_init_flag = 1;
 
 	return ESP_OK;
+}
+
+esp_err_t wi_nvs_op_config(int is_read){
+	static char buffer[sizeof(struct WI_Config_t) + 1];
+	wi_nvs_init();
+
+	nvs_handle_t nvs_handle;
+	esp_err_t err_code = nvs_open(WI_NVS_KEY_CONFIG, NVS_READWRITE, &nvs_handle);
+	if(err_code != ESP_OK){
+		ESP_LOGW(LOG_TAG, "从NVS中打开Config失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
+		return err_code;
+	}
+
+	err_code = ESP_OK;
+	if(is_read){
+		// NVS读取, 将NVS的内容覆盖到config
+		size_t length;
+		err_code = nvs_get_str(nvs_handle, WI_NVS_KEY_CONFIG, (char*)&WI_Config, &length);
+		if(err_code != ESP_OK)
+			ESP_LOGW(LOG_TAG, "从NVS中读取Config失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
+		
+	}else{
+		// NVS写入, 将config的内容写入NVS
+		memcpy(buffer, (char*)&WI_Config, sizeof(struct WI_Config_t));
+		buffer[sizeof(struct WI_Config_t)] = '\0';
+		err_code = nvs_set_str(nvs_handle, WI_NVS_KEY_CONFIG, buffer);
+		if(err_code != ESP_OK)
+			ESP_LOGW(LOG_TAG, "从NVS中写入Config失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
+		ESP_ERROR_CHECK(nvs_commit(nvs_handle));
+	}
+
+	nvs_close(nvs_handle);
+
+	return err_code;
+}
+
+esp_err_t wi_nvs_load_config(){
+	return wi_nvs_op_config(1);
 }
