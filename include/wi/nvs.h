751804e5632c4f4f1abdc1fa0f15bc9178399538
itlantu <it_lantu@outlook.com>
2025-10-29 21:19:42 +0800

    feat: 新增nvs读写全局配置的函数定义

--- include/wi/nvs.h
+++ include/wi/nvs.h
@@ -2,7 +2,12 @@
 #define WI_NVS_H
 
 #include "esp_err.h"
+#include "wi/config.h"
+
+#define WI_NVS_KEY_CONFIG "wi_config"
 
 esp_err_t wi_nvs_init();
+esp_err_t wi_nvs_load_config();
+esp_err_t wi_nvs_save_config();
 
 #endif // WI_NVS_H
