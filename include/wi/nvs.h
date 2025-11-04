ca53b6a7ea78038be0db1f9d6923e08e296eedbd
itlantu <it_lantu@outlook.com>
2025-11-05 00:52:32 +0800

    feat: 新增wi_nvs_load_history和wi_nvs_save_history函数实现历史查询记录功能

--- include/wi/nvs.h
+++ include/wi/nvs.h
@@ -1,13 +1,25 @@
 #ifndef WI_NVS_H
 #define WI_NVS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "esp_err.h"
 #include "wi/config.h"
 
 #define WI_NVS_KEY_CONFIG "wi_config"
+#define WI_NVS_KEY_HISTORY "wi_history"
 
 esp_err_t wi_nvs_init();
 esp_err_t wi_nvs_load_config();
 esp_err_t wi_nvs_save_config();
+
+esp_err_t wi_nvs_load_history(char* history_data);
+esp_err_t wi_nvs_save_history(const char* history_data);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // WI_NVS_H
--- src/nvs.c
+++ src/nvs.c
@@ -60,3 +60,47 @@ esp_err_t wi_nvs_op_config(int is_read){
 esp_err_t wi_nvs_load_config(){
 	return wi_nvs_op_config(1);
 }
+
+esp_err_t wi_nvs_load_history(char* history_data){
+	wi_nvs_init();
+
+	nvs_handle_t nvs_handle;
+	esp_err_t err_code = nvs_open(WI_NVS_KEY_CONFIG, NVS_READWRITE, &nvs_handle);
+	if(err_code != ESP_OK){
+		ESP_LOGW(LOG_TAG, "从NVS中打开History失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
+		return err_code;
+	}
+		
+	// NVS读取, 将NVS的内容覆盖到history
+	size_t length = 50;
+	err_code = nvs_get_str(nvs_handle, WI_NVS_KEY_HISTORY, history_data, &length);	
+	if(err_code != ESP_OK)
+		ESP_LOGW(LOG_TAG, "从NVS中读取History失败, 数据长度%d 错误原因: %s (%d)", length, esp_err_to_name(err_code), err_code);
+	else
+		ESP_LOGI(LOG_TAG, "读取成功, 数据长度: %d", length);
+	nvs_close(nvs_handle);
+
+	return err_code;
+}
+
+esp_err_t wi_nvs_save_history(const char* history_data){
+	wi_nvs_init();
+
+	nvs_handle_t nvs_handle;
+	esp_err_t err_code = nvs_open(WI_NVS_KEY_CONFIG, NVS_READWRITE, &nvs_handle);
+	if(err_code != ESP_OK){
+		ESP_LOGW(LOG_TAG, "从NVS中打开History失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
+		return err_code;
+	}
+
+	// NVS写入, 将history的内容写入NVS
+	err_code = nvs_set_str(nvs_handle, WI_NVS_KEY_HISTORY, history_data);
+	if(err_code != ESP_OK)
+		ESP_LOGW(LOG_TAG, "从NVS中写入History失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
+	ESP_ERROR_CHECK(nvs_commit(nvs_handle));
+	ESP_LOGI(LOG_TAG, "写入成功, 数据长度: %d", strlen(history_data));
+		
+	nvs_close(nvs_handle);
+
+	return err_code;
+}
