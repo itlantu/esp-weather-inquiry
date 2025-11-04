4d2f16df451742c46dabf1aeaa5475e8947da9cd
itlantu <it_lantu@outlook.com>
2025-11-05 01:02:47 +0800

    fix: 修复了wi_nvs_init函数被错误移除的问题

--- include/wi/nvs.h
+++ include/wi/nvs.h
@@ -10,6 +10,8 @@ extern "C" {
 
 #define WI_NVS_KEY_HISTORY "wi_history"
 
+esp_err_t wi_nvs_init();
+
 esp_err_t wi_nvs_load_history(char* history_data);
 esp_err_t wi_nvs_save_history(const char* history_data);
 
