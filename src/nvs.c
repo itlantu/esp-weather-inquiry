b5fb0b3db74f016f8a7a453216ed1b2762c0b941
itlantu <it_lantu@outlook.com>
2025-11-05 00:58:46 +0800

    fix: 修复了nvs key错误的问题; refactor: 移除了将config写入nvs的功能

--- src/nvs.c
+++ src/nvs.c
@@ -23,49 +23,11 @@ esp_err_t wi_nvs_init() {
 	return ESP_OK;
 }
 
-esp_err_t wi_nvs_op_config(int is_read){
-	static char buffer[sizeof(struct WI_Config_t) + 1];
-	wi_nvs_init();
-
-	nvs_handle_t nvs_handle;
-	esp_err_t err_code = nvs_open(WI_NVS_KEY_CONFIG, NVS_READWRITE, &nvs_handle);
-	if(err_code != ESP_OK){
-		ESP_LOGW(LOG_TAG, "从NVS中打开Config失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
-		return err_code;
-	}
-
-	err_code = ESP_OK;
-	if(is_read){
-		// NVS读取, 将NVS的内容覆盖到config
-		size_t length;
-		err_code = nvs_get_str(nvs_handle, WI_NVS_KEY_CONFIG, (char*)&WI_Config, &length);
-		if(err_code != ESP_OK)
-			ESP_LOGW(LOG_TAG, "从NVS中读取Config失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
-		
-	}else{
-		// NVS写入, 将config的内容写入NVS
-		memcpy(buffer, (char*)&WI_Config, sizeof(struct WI_Config_t));
-		buffer[sizeof(struct WI_Config_t)] = '\0';
-		err_code = nvs_set_str(nvs_handle, WI_NVS_KEY_CONFIG, buffer);
-		if(err_code != ESP_OK)
-			ESP_LOGW(LOG_TAG, "从NVS中写入Config失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
-		ESP_ERROR_CHECK(nvs_commit(nvs_handle));
-	}
-
-	nvs_close(nvs_handle);
-
-	return err_code;
-}
-
-esp_err_t wi_nvs_load_config(){
-	return wi_nvs_op_config(1);
-}
-
 esp_err_t wi_nvs_load_history(char* history_data){
 	wi_nvs_init();
 
 	nvs_handle_t nvs_handle;
-	esp_err_t err_code = nvs_open(WI_NVS_KEY_CONFIG, NVS_READWRITE, &nvs_handle);
+	esp_err_t err_code = nvs_open(WI_NVS_KEY_HISTORY, NVS_READWRITE, &nvs_handle);
 	if(err_code != ESP_OK){
 		ESP_LOGW(LOG_TAG, "从NVS中打开History失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
 		return err_code;
@@ -87,7 +49,7 @@ esp_err_t wi_nvs_save_history(const char* history_data){
 	wi_nvs_init();
 
 	nvs_handle_t nvs_handle;
-	esp_err_t err_code = nvs_open(WI_NVS_KEY_CONFIG, NVS_READWRITE, &nvs_handle);
+	esp_err_t err_code = nvs_open(WI_NVS_KEY_HISTORY, NVS_READWRITE, &nvs_handle);
 	if(err_code != ESP_OK){
 		ESP_LOGW(LOG_TAG, "从NVS中打开History失败, 错误原因: %s (%d)", esp_err_to_name(err_code), err_code);
 		return err_code;
