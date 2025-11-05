f4ce88d9e6d499a67409bd5d951da3b248cc9853
itlantu <it_lantu@outlook.com>
2025-11-05 14:12:14 +0800

    feat: 新增wi_url_decode函数

--- include/wi/parser/city_code.h
+++ include/wi/parser/city_code.h
@@ -7,9 +7,11 @@ extern "C" {
 
 #include "esp_err.h"
 
+esp_err_t wi_url_decode(char* result, const char* encode_data);
 esp_err_t wi_get_city_code(char* code, const char* name);
 esp_err_t wi_paser_post(const char* buffer, int data_length, char* city_name);
 esp_err_t wi_paser_get_str_find(const char* str, const char* find, size_t* end_pos);
+
 
 #ifdef __cplusplus
 }
